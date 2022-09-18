7a613dd8f6076553e8a830c38daf44a098d877f0
Ujjwal Sharma <>
2022-09-18 15:01:13 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@starting-up-again/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // its a set of file to be inlcuded cz issue ek file use hogi
+using namespace std; // std is standard functions
+int main() {
+  cout<<"ujjwal sharma";
+  return 0; //successfull termination of program
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // its a set of file to be inlcuded cz usse ek file use hogi to give input output and many other functions
+using namespace std; // std is standard functions
+int main() {
+  cout<<"ujjwal sharma";
+  return 0; //successfull termination of program
+}
